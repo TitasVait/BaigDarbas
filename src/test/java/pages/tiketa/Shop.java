7193d7b1d0829ec5ac836a8074bea613d0a90b73
titas <titasvaitkunas@gmail.com>
2022-04-08 02:32:57 +0300

    Added Shop page

--- src/test/java/pages/tiketa/Shop.java
+++ src/test/java/pages/tiketa/Shop.java
@@ -4,8 +4,20 @@ import org.openqa.selenium.By;
 import pages.Common;
 
 public class Shop {
-    public static void click() {
-        By locator = By.xpath("//*[@id='OnlyLogin']");
+    public static void clickOnBuy() {
+        By locator = By.xpath("//*[@class='btn btn-red a-red btn-default btn-lg btn-red text-uppercase pull-right']");
+        Common.clickElement(locator);
+    }
+    public static void clickOnBuyAgain() {
+        By locator = By.xpath("//*[@class='btn btn-red btn-default btn-sm text-uppercase']");
+        Common.clickElement(locator);
+    }
+    public static void clickOnNoLogin() {
+        By locator = By.xpath("//*[@id='btnNoLogin']");
+        Common.clickElement(locator);
+    }
+    public static void clickOnAvailableSector(){
+        By locator = By.cssSelector();
         Common.clickElement(locator);
     }
 }
--- src/test/java/tests/tiketa/Shop.java
+++ src/test/java/tests/tiketa/Shop.java
@@ -2,22 +2,33 @@ package tests.tiketa;
 
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import tests.BaseTest;
 
-public class Shop {
+public class Shop extends BaseTest {
     @BeforeClass
     public void openLink() {
         pages.Common.openLink("https://www.tiketa.lt/EN/parduotuve");
-//        pages.Common.cookiesAndReload();
+        pages.tiketa.Login.acceptAll();
     }
     @Test
     public void testShoppingBasket(){
         //1. Click any Buy button
         //2. Click Buy button again
-        //3. Click on a good sector
-        //4. Click Find tickets
-        //5. Click next
-        //6. Go to the home page by clicking the logo
-        //7.Check if there is a shopping basket
+        //3. Click buy without login
+        //4. Click on a good sector
+        //5. Click Find tickets
+        //6. Click next
+        //7. Go to the home page by clicking the logo
+        //8.Check if there is a shopping basket
+        pages.tiketa.Shop.clickOnBuy();
+        pages.tiketa.Shop.clickOnBuyAgain();
+        pages.tiketa.Shop.clickOnNoLogin();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pages.tiketa.Shop.clickOnAvailableSector();
 
     }
 }
